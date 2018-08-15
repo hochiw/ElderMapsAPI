a198f002fdae822b9a7c35dc305e27d367eba656
rico5504 <rico5504@gmail.com>
2018-08-15 15:29:06 +1000

    Redirected database info to Heroku

--- models/database.js
+++ models/database.js
@@ -1,12 +1,9 @@
 var mongoose = require('mongoose');
 
 // use "heroku local" to run the application or change the value
-//var db_url = process.env.dburl; // database url
-var db_url = "ds225608.mlab.com:25608/";
-//var db_account = process.env.dbac; // database account
-var db_account = "eldermaps";
-//var db_password= process.env.dbpw; // database password
-var db_password = "eldermap123";
+var db_url = process.env.dburl; // database url
+var db_account = process.env.dbac; // database account
+var db_password= process.env.dbpw; // database password
 var url = 'mongodb://'+db_account+':'+db_password+'@'+ db_url + db_account;
 
 mongoose.connect(url,{useNewUrlParser: true}).then(
